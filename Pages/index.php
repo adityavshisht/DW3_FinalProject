d1c903b21a43324b0165bc2104740adbb648fe2c
Aditya Sharma <adityavshisht2012005@gmail.com>
2025-03-20 19:03:42 -0400

    i pasted all the pages inside a folder Pages

---
+++ Pages/index.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+include 'header.php';
+?>
+<main>
+    <h2>Welcome to ReTechX</h2>
+    <p>Buy and sell used electronics securely and conveniently.</p>
+</main>
+<?php include 'footer.php'; ?>
--- index.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-include 'header.php';
-?>
-<main>
-    <h2>Welcome to ReTechX</h2>
-    <p>Buy and sell used electronics securely and conveniently.</p>
-</main>
-<?php include 'footer.php'; ?>
